b284c94716af5cf48f91928e7aa97d8d79f98032
ASUTOSH PANIGRAHI <ASUTOSH PANIGRAHI@DESKTOP-MUI4NLR>
2025-06-27 00:06:40 +0530

    Hello

---
+++ NamithaFramework0806/src/test/java/calenderTest/ToCreateCalenderTest.java
@@ -0,0 +1,11 @@
+package calenderTest;
+
+public class ToCreateCalenderTest {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Your Framework is finished hahahaha");
+
+	}
+
+}
